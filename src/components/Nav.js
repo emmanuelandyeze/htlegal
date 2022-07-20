1385cb98663634dc683e7287899aaf4e4ed6f663
emmanuelandyeze <3riveafrica.com>
2022-07-20 12:14:36 +0100

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,151 @@
+import { NavLink, Outlet } from "react-router-dom";
+import React,{useState} from 'react';
+import {FiAlignRight,FiXCircle,FiChevronDown } from "react-icons/fi";
+import "./Nav.css";
+
+const Nav = () => {
+	const [isMenu, setisMenu] = useState(false);
+  const [isResponsiveclose, setResponsiveclose] = useState(false);
+  const toggleClass = () => {
+    setisMenu(isMenu === false ? true : false);
+    setResponsiveclose(isResponsiveclose === false ? true : false);
+  };
+  let boxClass = ["nav__container"];
+  if(isMenu) {
+    boxClass.push('responsive__nav__show');
+  }else{
+    boxClass.push('');
+  }
+  const [isMenuSubMenu, setMenuSubMenu] = useState(false);
+  const toggleSubmenu = () => {
+    setMenuSubMenu(isMenuSubMenu === false ? true : false);
+  };
+  let boxClassSubMenu = ["sub__menus"];
+  if(isMenuSubMenu) {
+    boxClassSubMenu.push('sub__menus__Active');
+  }else {
+    boxClassSubMenu.push('');
+  }
+  return (
+		<div className="nav">
+			<img
+				src="/htlogo.png"
+				height={100}
+				width={100}
+				alt=""
+			/>
+			{isResponsiveclose === true ? (
+				<>
+					<span
+						className="menubar__button"
+						style={{ display: 'none' }}
+						onClick={toggleClass}
+					>
+						{' '}
+						<FiXCircle />{' '}
+					</span>
+				</>
+			) : (
+				<>
+					<span
+						className="menubar__button"
+						style={{ display: 'none' }}
+						onClick={toggleClass}
+					>
+						{' '}
+						<FiAlignRight />{' '}
+					</span>
+				</>
+			)}
+			<ul className={boxClass.join(' ')}>
+				{/* Approach #1 --- Active  */}
+				<li>
+					<NavLink
+						onClick={toggleClass}
+						className={({ isActive }) =>
+							isActive ? 'active' : ''
+						}
+						to="/"
+					>
+						Home
+					</NavLink>
+				</li>
+
+				{/* Approach #2 --- Active  */}
+				<li>
+					<NavLink
+						onClick={toggleClass}
+						style={({ isActive }) => {
+							return {
+								color: isActive ? 'active' : '',
+							};
+						}}
+						to="/about"
+					>
+						About Us
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						onClick={toggleClass}
+						style={({ isActive }) => {
+							return {
+								color: isActive ? 'active' : '',
+							};
+						}}
+						to="/user"
+					>
+						Our Services
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						onClick={toggleClass}
+						className={({ isActive }) =>
+							isActive ? 'active' : ''
+						}
+						to="/invoice"
+					>
+						Our Team
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						onClick={toggleClass}
+						className={({ isActive }) =>
+							isActive ? 'active' : ''
+						}
+						to="/object_route"
+					>
+						Contact Us
+					</NavLink>
+				</li>
+				
+				<li>
+					<NavLink
+						// onClick={toggleClass}
+						// className={({ isActive }) =>
+						// 	isActive ? 'active' : ''
+						// }
+						to="/search"
+						style={{
+							backgroundColor: '#800000',
+							color: '#fff',
+							padding: '.5rem 1rem',
+							borderRadius: '5px',
+						}}
+					>
+						{' '}
+						Book an Appointment{' '}
+					</NavLink>
+				</li>
+				<Outlet />
+			</ul>
+			{/* <div>
+        <a href="/" className="appointBtn">Book an Appointment</a>
+      </div> */}
+		</div>
+	);
+};
+
+export default Nav;
---
+++ src/page/Home.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import './Style.css'
+
+const Home = () => {
+  return (
+		<div>
+			<div className="landing">
+				<div className="landing-text">
+					<h1>
+						Accessible and cost effective <br /> legal
+						services for all.
+					</h1>
+					<h3>
+						At HT Legal, we are dedicated to understanding
+						the uniqueness of existing <br /> as well as
+						potential clients and this helps us to tailor
+						our work to their distictive needs.
+					</h3>
+					<div>
+						<a href="/" className="appointBtn">
+							Book an Appointment
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="about">
+				<div className="about-heading">
+					<h3>Experience. Respect. Result</h3>
+					<h1>You are in the Right Place</h1>
+				</div>
+				<div className="about-text">
+					<p>
+						At HT Legal, we are dedicated to understanding
+						the uniqueness of existing as well as potential
+						clients and this helps us to tailor our work to
+						their distictive needs. We place great emphasis
+						on professionalism, competence, and
+						respectability and we strive to build the
+						feeling of confidence and trust together with
+						our clients so that they can contact us anytime
+						and anywhere. Over many years, our team has
+						gained the experience and expertise necessary to
+						make your journey with us as seamless as
+						possible. We offer cost effective legal
+						solutions to micro, small and medium enterprises
+						as well as cater to individual legal needs.
+					</p>
+				</div>
+				<div>
+					<a href="/" className="appointBtn">
+						About Us
+					</a>
+				</div>
+			</div>
+			<div className="services">
+				<div className="about-heading">
+					<h3>What we do.</h3>
+					<h1>Our Services</h1>
+				</div>
+				<div className="cards">
+					<div className="card">
+						<div className="service-header">
+							<h1>EMPLOYMENT & LABOUR RELATIONS</h1>
+						</div>
+						<hr />
+						<div className="service-text">
+							<p>
+								We offer services to clients on a wide range
+								of employment related matters. You can reach
+								out to us on matters relating to Employment
+								Onboarding; Confidentiality Agreements;
+								Constructive Dismissal; Employment
+								Contracts; Non-compete Agreements.
+							</p>
+						</div>
+					</div>
+					<div className="card">
+						<div className="service-header">
+							<h1>CORPORATE & BUSINESS ADVISORY</h1>
+						</div>
+						<hr />
+						<div className="service-text">
+							<p>
+								HT Legal is equipped to render comprehensive
+								business advisory services for clients in
+								all aspects of corporate and commercial
+								matters, including corporate governance,
+								formation of companies, legal audit,
+								taxation,contract drafting and review, real
+								estate advisory and general commercial
+								advisory
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="cards">
+					<div className="card">
+						<div className="service-header">
+							<h1>ESTATE ADMINISTRATION</h1>
+						</div>
+						<hr />
+						<div className="service-text">
+							<p>
+								Have you been appointed as a trust or will
+								beneficiary? Have you lost a loved one who
+								died without a will and need assistance
+								finding out what to do next? We will assist
+								you. We also offer services related to:
+								Obtaining Probate Letters of Administration
+								Drafting of Wills/Codicil
+							</p>
+						</div>
+					</div>
+					<div className="card">
+						<div className="service-header">
+							<h1>TECHNOLOGY</h1>
+						</div>
+						<hr />
+						<div className="service-text">
+							<p>
+								Are you a software developer? Does your work
+								entail the development of mobile
+								applications? We offer legal advisory and
+								contract drafting services for software
+								licensing, software maintenance and mobile
+								app development.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="choose">
+				<div className="about-heading">
+					<h1>Why Choose Us?</h1>
+					<p>
+						Professional excellence is the hallmark of our
+						practice. With HT Legal, you can expect timely
+						resolution, excellent delivery and value for
+						money spent.
+					</p>
+					<p>
+						We believe ourselves to be properly positioned
+						in a challenging and constantly changing market,
+						where we help our clients overcome their most
+						complex legal issues. By working with HT Legal,
+						you get more than professional legal
+						representation, you get premium value for
+						expended resources and solutions delivered
+						seamlessly.
+					</p>
+					<p>
+						We are consistent, patient and professional,
+						giving each new case our undivided attention
+						whilst walking you through each step of the
+						process.
+					</p>
+				</div>
+				<div style={{marginTop: '2rem'}}>
+					<a href="/" className="appointBtn">
+						Book an appointment
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Home
